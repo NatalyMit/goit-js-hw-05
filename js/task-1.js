b2fafbcaec96422f3015b276fe27a1f0f73a06bd
NatalyMit <mitenkova91@icloud.com>
2023-11-24 11:40:33 +0100

    task-1-3, add task-4

--- js/task-1.js
+++ js/task-1.js
@@ -1,21 +1,50 @@
 'use strict';
 
-function isEnoughCapacity(products, containerSize) {
-  let totalProd = 0;
-  const values = Object.values(products);
-  for (const value of values) {
-    totalProd += value;
-  }
-  if (totalProd <= containerSize) {
-    return true;
-  }
-  return false;
-}
-
-console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
-
-console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
-
-console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
-
-console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
+const getUserNames = users => {
+  let userNamesArrey = [];
+  users.forEach(user => {
+    if (user.name) {
+      userNamesArrey.push(user.name);
+    }
+  });
+  return userNamesArrey;
+};
+console.log(
+  getUserNames([
+    {
+      name: 'Moore Hensley',
+      email: 'moorehensley@indexia.com',
+      balance: 2811,
+    },
+    {
+      name: 'Sharlene Bush',
+      email: 'sharlenebush@tubesys.com',
+      balance: 3821,
+    },
+    {
+      name: 'Ross Vazquez',
+      email: 'rossvazquez@xinware.com',
+      balance: 3793,
+    },
+    {
+      name: 'Elma Head',
+      email: 'elmahead@omatom.com',
+      balance: 2278,
+    },
+    {
+      name: 'Carey Barr',
+      email: 'careybarr@nurali.com',
+      balance: 3951,
+    },
+    {
+      name: 'Blackburn Dotson',
+      email: 'blackburndotson@furnigeer.com',
+      balance: 1498,
+    },
+    {
+      name: 'Sheree Anthony',
+      email: 'shereeanthony@kog.com',
+      balance: 2764,
+    },
+  ])
+); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
